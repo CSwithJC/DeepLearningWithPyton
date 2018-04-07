f8817be9e0ba54e28606cc22e890d49351ca2a1d
jean28 <jean.mendez2@upr.edu>
2018-04-06 23:48:43 -0400

    Updated README.txt and added training to example 01.

---
+++ README.txt
@@ -0,0 +1,2 @@
+Commented code samples from the book "Deep Learning with Python", by François Chollet. Mostly to be used for personal
+reference in the future.
--- chapter_02_math_of_neural_networks/example_01_mnist_neural_net.py
+++ chapter_02_math_of_neural_networks/example_01_mnist_neural_net.py
@@ -1,7 +1,8 @@
 """Simple example of a Neural Net classifier for the MNIST dataset using Keras
 """
-from keras.datasets import mnist
 from keras import layers, models
+from keras.datasets import mnist
+from keras.utils import to_categorical
 
 # Load the MNIST dataset as test and training data
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -35,3 +36,20 @@ network.compile(optimizer='rmsprop',
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
 
+# Reshape MNIST data so that it is in the shape the network expects.
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+# Categorically encode the labels
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# Train the model
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# Check how model performs on test data
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc', test_acc)
